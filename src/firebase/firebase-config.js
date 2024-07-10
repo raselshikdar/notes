e31562e8c6347af9ffa93af57d139545a0f15802
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-07-10 11:31:18 +0600

    Update firebase-config.js

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,19 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyBL8EKntGLCdMOqoUmt0ERhM7fqxCX6WR0",
-	authDomain: "ripplenote-15.firebaseapp.com",
-	projectId: "ripplenote-15",
-	storageBucket: "ripplenote-15.appspot.com",
-	messagingSenderId: "603590725350",
-	appId: "1:603590725350:web:c099b8952dc117c4e77527"
+  apiKey: "AIzaSyDU-thzFz47d1YvnzxCvLKtSThl8DhLoQA",
+  authDomain: "rs-notes.firebaseapp.com",
+  projectId: "rs-notes",
+  storageBucket: "rs-notes.appspot.com",
+  messagingSenderId: "702940054474",
+  appId: "1:702940054474:web:7cf055794b9ec991fe8533",
+  measurementId: "G-FJHH8G2RT7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
-// Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
